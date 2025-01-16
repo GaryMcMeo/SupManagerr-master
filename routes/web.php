83e71650871f1a24292d300ead9bb68463e6a2bc
GaryMcMeo <107529089+GaryMcMeo@users.noreply.github.com>
2025-01-16 13:16:55 +0800

    Update Home Page

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,11 @@ require __DIR__.'/auth.php';
 
 Route::middleware(['auth'])->group(function () {
     
+    // Home route
+    Route::get('/home', function () {
+        return view('welcome');
+    })->name('home');
+
     // Profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
